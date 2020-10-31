3b8de9421f54a8ee11e425ccb027be386040fec5
harshaliSharma <harshalisharma2716@gmail.com>
2020-10-31 15:18:30 +0530

    Updates

---
+++ precache-manifest.8285804c06c692033c45201accd19a4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbc82fe4f5fe0286cee9232ef3c88e79",
+    "url": "/reactlivewebsite/index.html"
+  },
+  {
+    "revision": "e5b309c08a15f10b4c2a",
+    "url": "/reactlivewebsite/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "5c70df03a100cb1f377c",
+    "url": "/reactlivewebsite/static/css/main.fba6208a.chunk.css"
+  },
+  {
+    "revision": "e5b309c08a15f10b4c2a",
+    "url": "/reactlivewebsite/static/js/2.33300fd8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactlivewebsite/static/js/2.33300fd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c70df03a100cb1f377c",
+    "url": "/reactlivewebsite/static/js/main.0f9da802.chunk.js"
+  },
+  {
+    "revision": "f3421ab5bf184d3f6fdd",
+    "url": "/reactlivewebsite/static/js/runtime-main.fa23aac9.js"
+  }
+]);
