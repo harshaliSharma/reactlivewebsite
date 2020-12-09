82448c5b9e6ae1388d86789a1ebe9faec8e271b6
harshaliSharma <71386111+harshaliSharma@users.noreply.github.com>
2020-12-09 20:02:27 +0530

    Update Menu.jsx

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -13,12 +13,9 @@ const Menu=()=>
   
 <nav className="navbar navbar-expand-lg navbar-light bg-light">
  <NavLink className="navbar-brand" to="/">Harshali</NavLink>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" 
-  data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
+  
 
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+  <div >
     <ul className="navbar-nav ml-auto">
       <li className="nav-item active">
         <NavLink className="nav-link"  activeClassName="menu_active" exact to="/">Home <span className="sr-only">(current)</span></NavLink>
